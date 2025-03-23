fa5c671455f508fdf833d8cc289c3c3aeb67ed7a
hedrickantonio <rhantoni.cuba@gmail.com>
2025-03-23 20:20:13 +0000

    random card

--- src/app.js
+++ src/app.js
@@ -184,5 +184,107 @@ const sumWithInitial = anArray[0].reduce(
     
       console.log(sumWithInitial + sumWithInitial1 + sumWithInitial2)
 
-
+      let myList = document.getElementsByTagName("tr");
+      
      
+      myList[3].style.background = "red";   // cambia el primer elemento a rojo
+     
+      document.getElementsByTagName("p")[0].innerHTML = "abc";
+// innerHTML puede usarse para insertar contenido de texto o HTML, esto crea texto dentro de un elemento <div>
+
+let divElem = document.getElementById("myFirstDiv");
+let myNewHOne = document.createElement("h1");
+let t = document.createTextNode("Hello World");
+myNewHOne.appendChild(t);  // Esta linea añade el texto al <h1>  
+divElem.appendChild(myNewHOne); // Esto añade el <h1> al elemento "myFirstDiv"
+
+let tableElement = document.getElementById("people1");
+let NewTr= document.createElement("tr");
+let NewTh= document.createElement("td");
+let NewTh1= document.createElement("td");
+let NewTh2= document.createElement("td");
+let testo = document.createTextNode ("josefa")
+let testo1 = document.createTextNode ("josefa1")
+let testo2 = document.createTextNode ("josefa2")
+NewTh.appendChild(testo);
+NewTh1.appendChild(testo1);
+NewTh2.appendChild(testo2);
+NewTr.appendChild(NewTh);
+NewTr.appendChild(NewTh1);
+NewTr.appendChild(NewTh2);
+tableElement.appendChild(NewTr);
+
+let element = document.getElementById("people1");
+while (element.firstChild) {
+    element.removeChild(element.firstChild);
+}
+
+let myUL = document.querySelector("ul");
+
+let names = ['Tom Bombadil', 'Aragorn', 'Arwen', 'Boromir', 'Faramir', 'Saruman', 'Legolas'];
+
+names.map(name=>{
+	myUL.innerHTML += `<li>${name}</li>`; 
+});
+
+let LIs = document.querySelectorAll("li");
+
+LIs.forEach(LI => LI.addEventListener('contextmenu', (e)=>{
+	LI.parentNode.removeChild(LI);
+}));
+
+let counter = 0;
+let caja = document.getElementById("testeo");
+let increase = document.getElementById("increase");
+let H1loading= document.createElement("h1");
+let Pdeloading = document.createTextNode ("Loading");
+caja.appendChild(H1loading);
+H1loading.appendChild(Pdeloading);
+
+increase.addEventListener('click', (e)=>{
+    counter ++;
+  
+
+    H1loading.innerHTML = "The counter value is "+counter;
+})
+
+let decrease = document.getElementById("decrease");
+
+decrease.addEventListener('click', (e)=>{
+    counter --;
+  
+
+    H1loading.innerHTML = "The counter value is "+counter;
+})
+
+
+
+//-----AQUI EMPIEZA EL RANDOM CARD-----////
+let NumeroCartas = [1,2,3,4,5,6,7,8,9,10,11,12];
+let Palos = ["♦", "♥", "♠", "♣"];
+
+const Randomnize =(arr)=>{
+    return Math.floor(Math.random()* arr.length)
+}
+console.log(Randomnize(NumeroCartas));
+console.log(Randomnize(Palos));
+
+const resultadoCarta = arr => {
+    return arr[Randomnize(arr)]
+      }
+      console.log(resultadoCarta(NumeroCartas))
+      console.log(resultadoCarta(Palos))
+
+let simbolo = document.getElementById("simbolo");
+let NumeroCarta = document.getElementById("NumeroCarta");
+let reset = document.getElementById("reset");
+
+
+reset.addEventListener('click', (e)=>{
+    
+  
+
+    NumeroCarta.innerHTML = resultadoCarta(NumeroCartas);
+    simbolo.innerHTML = resultadoCarta(Palos);
+
+})
